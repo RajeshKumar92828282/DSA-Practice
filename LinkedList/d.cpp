9f4d9cc8a64a93cd4545e30307440a92567c25e1
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-03-07 22:20:36 +0530

    create doubly linked list

---
+++ LinkedList/d.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    node*prev;
+
+    Node(int value){
+        this->data=value;
+        prev=NULL;
+        next=NULL:
+
+    }
+};
+
+class Linkedlist{
+     private:
+     Node* head;
+     public:
+     Linkedlist(){
+       head=NULL:
+     }
+
+      void insertbeign(int value){
+        Node* newnode=new Node(data);
+      }
+}
